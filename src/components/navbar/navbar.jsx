f7e216dbbe186c6b833e08a04bd4ec3b3f2879a2
72730882 <faizamohammed72730882@gmail.com>
2024-07-19 19:17:22 -1200

    Home page

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -20,12 +20,12 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="App.js">Navbar</Link>
+                <Link className="navbar-brand" href="App.js">Double B</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0 fw-bold h-font">
                         <li className="nav-item">
                         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
