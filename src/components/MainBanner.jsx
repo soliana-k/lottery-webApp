3242a2b68b23930947ed54671362292444eff177
72730882 <faizamohammed72730882@gmail.com>
2024-07-19 19:43:24 -1200

    home page

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import './styles.css';
 const MainBanner = () => {
   return (
-    <section id="home" className="main-banner">
-      <h1>Catchy Slogan or Tagline</h1>
-      <button className="cta-button">Play Now</button>
-    </section>
+    <div className="container">
+      <div className="banner">
+        <div className="banner-text text-center">
+        <h2 className="mt-5 pt-4 mb-4  fw-bold h-font">Bet the Number, Win Big Prizes</h2>
+     <p>Play Now and Try Your Luck!</p>
+     <button>Play Now</button>
+
+        </div>
+      
+      </div>
+    </div>
   );
 }
 
