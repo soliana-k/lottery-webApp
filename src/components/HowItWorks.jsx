c5fed210c4d3c6d79c73e5974696ce39405bf386
72730882 <faizamohammed72730882@gmail.com>
2024-07-19 20:22:22 -1200

    home page

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles.css';
 
 const HowItWorks = () => {
   return (
     <section id="how-it-works" className="how-it-works">
-      <h2>How It Works</h2>
-      <p>Brief explanation of the process...</p>
+       <h2 className="mt-5 pt-4 mb-4 text-center fw-bold h-font">How It Works</h2>
+      <p className="text-center">Learn How easy it is to participate and win</p>
       <div className="steps">
         <div className="step">
           <img src="/public/assets/a.jpg" alt="Step 1" />
--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import './styles.css';
 const MainBanner = () => {
   return (
-    <div className="container">
-      <div className="banner">
+    <div className="main-banner">
+      <div className="banner ">
         <div className="banner-text text-center">
         <h2 className="mt-5 pt-4 mb-4  fw-bold h-font">Bet the Number, Win Big Prizes</h2>
-     <p>Play Now and Try Your Luck!</p>
-     <button>Play Now</button>
-
+        <p>Play Now and Try Your Luck!</p>
+        <button className="btn">Play Now</button>
         </div>
-      
       </div>
     </div>
   );
