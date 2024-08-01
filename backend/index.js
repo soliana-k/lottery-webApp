71ae7ce4e09260545cd14b442a35b024fb08531b
Fevenes <fevaesa82@gmail.com>
2024-08-01 12:11:23 -0700

    contact

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRouter from "./routes/user_route.js";
+import contactRouter from "./routes/contact_route.js";
 
 
 
-dotenv.config({})
+dotenv.config();
+
+
 const app = express();
 
 
@@ -23,12 +26,15 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 
 // api
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/contact",contactRouter);
 
-app.listen(PORT, ()=>{
-    connectDB();
+
+app.listen(PORT, ()=> {
     console.log(`Server running at port ${PORT}`);
-})
+});
