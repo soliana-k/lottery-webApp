07084365c6dfacb9cae8d414204d1484b7e76694
72730882 <faizamohammed72730882@gmail.com>
2024-08-03 10:08:32 -1200

    update

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,6 +2,11 @@ import "./home.css";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Login from "../../components/login/Login";
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+
+
 
 const Home = () => {
   return (
@@ -9,10 +14,28 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        
-        
-
-
+         <div className="Homecontent">
+          <div className="main-cards">
+            <div className="card">
+              <div className="card-inner">
+                <h3>Prizes Managment</h3>
+                <MonetizationOnIcon className="icon"/>
+             </div>
+            </div>
+            <div className="card">
+              <div className="card-inner">
+                <h3>Number Managment</h3>
+                <BorderColorIcon className="icon"/>
+                </div>
+            </div>
+            <div className="card">
+              <div className="card-inner">
+                <h3>Content Managment</h3>
+                <ContentPasteIcon className="icon"/>
+                </div>
+            </div>
+          </div>
+         </div>
       </div>
     </div>
   );
