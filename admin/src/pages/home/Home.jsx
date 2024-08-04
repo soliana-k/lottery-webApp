c24c0395dab5ff29ecbfe0f33314266aec5a1f75
72730882 <faizamohammed72730882@gmail.com>
2024-08-03 14:21:32 -1200

    updatte

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,23 +1,39 @@
 import "./home.css";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
-import BorderColorIcon from '@mui/icons-material/BorderColor';
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MessageIcon from '@mui/icons-material/Message';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MessageIcon from "@mui/icons-material/Message";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const Home = () => {
   const notifications = [
-    { id: 1, message: 'The next lottery draw is scheduled for 2024-08-01.', date: '2024-07-25' },
-    { id: 2, message: 'The results for the July draw have been published.', date: '2024-07-21' },
+    {
+      id: 1,
+      message: "The next lottery draw is scheduled for 2024-08-01.",
+      date: "2024-07-25",
+    },
+    {
+      id: 2,
+      message: "The results for the July draw have been published.",
+      date: "2024-07-21",
+    },
   ];
 
   const messages = [
-    { id: 1, message: 'A new batch of lottery tickets is available for sale.', date: '2024-07-24' },
-    { id: 2, message: 'Update: The prize fund for the August draw has been increased.', date: '2024-07-23' },
+    {
+      id: 1,
+      message: "A new batch of lottery tickets is available for sale.",
+      date: "2024-07-24",
+    },
+    {
+      id: 2,
+      message: "The prize fund for the August draw has been increased.",
+      date: "2024-07-23",
+    },
   ];
 
   return (
@@ -29,37 +45,53 @@ const Home = () => {
           <div className="main-cards">
             <div className="card">
               <div className="card-inner">
-                <MonetizationOnIcon className="card_icon" />
                 <div className="card-content">
-                  <h5>Prizes Management</h5>
+                  <h5>
+                    <MonetizationOnIcon className="card_icon" />
+                    Prizes Management
+                  </h5>
+
                   <p className="card-description">
-                    Manage and update prize details here. You can add new prizes or modify existing ones.
+                    Manage and update prize details here. You can add new prizes
+                    or modify existing ones.
                   </p>
-                  <Link to="/prizes" className="card-link">Go to Prizes Management</Link>
+                  <Link to="/prizes" className="card-link">
+                    Go to Prizes Management
+                  </Link>
                 </div>
               </div>
             </div>
             <div className="card">
               <div className="card-inner">
-                <BorderColorIcon className="card_icon" />
                 <div className="card-content">
-                  <h5>Number Management</h5>
+                  <h5>
+                  <BorderColorIcon className="card_icon" />
+
+                    Number Management</h5>
                   <p className="card-description">
-                    View and manage selected and remaining numbers. Update number allocations as needed.
+                    View and manage selected and remaining numbers. Update
+                    number allocations as needed.
                   </p>
-                  <Link to="/numbers" className="card-link">Go to Number Management</Link>
+                  <Link to="/numbers" className="card-link">
+                    Go to Number Management
+                  </Link>
                 </div>
               </div>
             </div>
             <div className="card">
               <div className="card-inner">
-                <ContentPasteIcon className="card_icon" />
                 <div className="card-content">
-                  <h5>Content Management</h5>
+                  <h5>
+                  <ContentPasteIcon className="card_icon" />
+
+                    Content Management</h5>
                   <p className="card-description">
-                    Update and manage website content. This includes text, images, and other media.
+                    Update and manage website content. This includes text,
+                    images, and other media.
                   </p>
-                  <Link to="/content" className="card-link">Go to Content Management</Link>
+                  <Link to="/content" className="card-link">
+                    Go to Content Management
+                  </Link>
                 </div>
               </div>
             </div>
@@ -74,17 +106,20 @@ const Home = () => {
                     <h5>Notifications</h5>
                   </div>
                   <div>
-                    <i className='bx bx-filter ml-2'></i>
+                    <i className="bx bx-filter ml-2"></i>
                   </div>
                 </Card.Header>
                 <Card.Body>
                   <ul className="notification-list">
-                    {notifications.map(notification => (
-                      <li key={notification.id} className="notification-item d-flex align-items-center">
-                       
+                    {notifications.map((notification) => (
+                      <li
+                        key={notification.id}
+                        className="notification-item d-flex align-items-center"
+                      >
                         <p>{notification.message}</p>
-                        <span className="notification-date">{notification.date}</span>
-                       
+                        <span className="notification-date">
+                          {notification.date}
+                        </span>
                       </li>
                     ))}
                   </ul>
@@ -100,17 +135,20 @@ const Home = () => {
                     <h5>Messages</h5>
                   </div>
                   <div>
-                    <i className='bx bx-filter ml-2'></i>
+                    <i className="bx bx-filter ml-2"></i>
                   </div>
                 </Card.Header>
                 <Card.Body>
                   <ul className="notification-list">
-                    {messages.map(message => (
-                      <li key={message.id} className="notification-item d-flex align-items-center">
-                     
+                    {messages.map((message) => (
+                      <li
+                        key={message.id}
+                        className="notification-item d-flex align-items-center"
+                      >
                         <p>{message.message}</p>
-                        <span className="notification-date">{message.date}</span>
-                        
+                        <span className="notification-date">
+                          {message.date}
+                        </span>
                       </li>
                     ))}
                   </ul>
