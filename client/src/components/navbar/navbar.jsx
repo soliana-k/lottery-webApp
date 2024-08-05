fd60ff471d3dfc1cecd56d9c39c7d8c39d66c2dc
72730882 <faizamohammed72730882@gmail.com>
2024-08-04 18:34:41 -1200

    update

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/How_it_works">
+              <Link className="nav-link" to="/How_it_works"> 
                 How It Works
               </Link>
             </li>
