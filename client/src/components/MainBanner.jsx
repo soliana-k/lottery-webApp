27b756d585f5cb317912f073d9df5ff5841d0eb9
Fafi03 <138723875+Fafi03@users.noreply.github.com>
2024-08-05 12:21:40 +0300

    update

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -8,7 +8,7 @@ const MainBanner = () => {
         <div className="banner-text text-center">
         <h2 className="mt-5 pt-4 mb-4  fw-bold h-font">Bet the Number, Win Big Prizes</h2>
         <p>Play Now and Try Your Luck!</p>
-        <Link to="/play" className="btn btn-primary">Play now</Link>
+        <Link to="/Prizes" className="btn btn-primary">Play now</Link>
         </div>
       </div>
     </div>
