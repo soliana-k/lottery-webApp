8dd896c699a8d63d2c7d774cddc356f85d01e1dc
Feven <139780506+Fevenes@users.noreply.github.com>
2024-08-17 14:20:42 -0700

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Sidebar from './components/sidebar/sidebar';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/home/Home';
 import Dashboard from './pages/dashboard/Dashboard';
 import Payment from './pages/payment/Payment';
