8abc1e8b87dd9df778c3e140f666daa30940d8c7
72730882 <faizamohammed72730882@gmail.com>
2024-08-18 19:52:46 -1200

    update

--- admin/src/pages/settings/Settings.jsx
+++ admin/src/pages/settings/Settings.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 function Settings() {
   return (
     <div>
       <h1>Settings</h1>
+      <Link className="nav-link" to="/adminRegistration">
+                adminRegistration
+              </Link>
     </div>
   );
 }
