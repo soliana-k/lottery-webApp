a3c6c69a4af194ee672103d2098cd20d1008ef2b
kalkidan <kalukassahun29@gmail.com>
2024-08-23 18:42:42 +0300

    working on ui history

--- admin/src/pages/NumberManagement/history.js
+++ admin/src/pages/NumberManagement/history.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Table, Tab, Nav } from 'react-bootstrap';
+import { Container, Grid, Typography, Tabs, Tab, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Breadcrumbs from '../../breadcrumb';
-import axios from 'axios'; 
+import axios from 'axios';
 
 const History = () => {
   const [key, setKey] = useState('draws');
@@ -11,12 +11,10 @@ const History = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        
         const drawsResponse = await axios.get('http://localhost:8000/api/v1/admin/draws');
         setPastDraws(drawsResponse.data);
 
-        
-        const logsResponse = await axios.get('http://localhost:8000/api/v1/admin/draws');
+        const logsResponse = await axios.get('http://localhost:8000/api/v1/admin/logs');
         setLogs(logsResponse.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -36,69 +34,105 @@ const History = () => {
           { label: 'History', href: '/history' }
         ]}
       />
-      <Row className="my-4">
-        <Col md={12}>
-          <h2 className="mb-4">Draw History</h2>
-          <Tab.Container id="left-tabs-example" activeKey={key} onSelect={(k) => setKey(k)}>
-            <Nav variant="tabs">
-              <Nav.Item>
-                <Nav.Link eventKey="draws">Past Draws</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="logs">Logs</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
-              <Tab.Pane eventKey="draws">
-                <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Date</th>
-                      <th>Time</th>
-                      <th>Winner</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+      <Grid container spacing={4} className="my-4">
+        <Grid item xs={12}>
+        <Typography variant="h5" gutterBottom>
+              Draws Ledger
+            </Typography>
+            <Typography variant="body1" color="textSecondary" paragraph>
+              Review Historical Draws and Logs in Detail
+            </Typography>
+          <Paper elevation={3}>
+            <Tabs
+              value={key}
+              onChange={(event, newValue) => setKey(newValue)}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+              sx={{
+                '& .MuiTab-root': {
+                  '&:hover': {
+                    backgroundColor: 'transparent', // Disable hover background color
+                  },
+                },
+              }}
+            >
+              <Tab label="Past Draws" value="draws" />
+              <Tab label="Logs" value="logs" />
+            </Tabs>
+            <TabPanel value={key} index="draws">
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Time</TableCell>
+                      <TableCell>Winner</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
                     {pastDraws.map((draw) => (
-                      <tr key={draw._id}>
-                        <td>{draw._id}</td>
-                        <td>{new Date(draw.date).toLocaleDateString()}</td>
-                        <td>{draw.time}</td>
-                        <td>{draw.winner ? draw.winner.name : 'N/A'}</td> {/* Adjust according to your schema */}
-                      </tr>
+                      <TableRow key={draw._id}>
+                        <TableCell>{draw._id}</TableCell>
+                        <TableCell>{new Date(draw.date).toLocaleDateString()}</TableCell>
+                        <TableCell>{draw.time}</TableCell>
+                        <TableCell>{draw.winner ? draw.winner.name : 'N/A'}</TableCell>
+                      </TableRow>
                     ))}
-                  </tbody>
+                  </TableBody>
                 </Table>
-              </Tab.Pane>
-              <Tab.Pane eventKey="logs">
-                <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Action</th>
-                      <th>Timestamp</th>
-                      <th>Details</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+              </TableContainer>
+            </TabPanel>
+            <TabPanel value={key} index="logs">
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell>Action</TableCell>
+                      <TableCell>Timestamp</TableCell>
+                      <TableCell>Details</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
                     {logs.map((log) => (
-                      <tr key={log._id}>
-                        <td>{log._id}</td>
-                        <td>{log.action}</td>
-                        <td>{new Date(log.timestamp).toLocaleString()}</td>
-                        <td>{log.details}</td>
-                      </tr>
+                      <TableRow key={log._id}>
+                        <TableCell>{log._id}</TableCell>
+                        <TableCell>{log.action}</TableCell>
+                        <TableCell>{new Date(log.timestamp).toLocaleString()}</TableCell>
+                        <TableCell>{log.details}</TableCell>
+                      </TableRow>
                     ))}
-                  </tbody>
+                  </TableBody>
                 </Table>
-              </Tab.Pane>
-            </Tab.Content>
-          </Tab.Container>
-        </Col>
-      </Row>
+              </TableContainer>
+            </TabPanel>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Grid container>
+          {children}
+        </Grid>
+      )}
+    </div>
+  );
+}
 
 export default History;
