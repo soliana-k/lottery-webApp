666830d4eb74d9502ab992ce55258ebf38a0bcc5
72730882 <faizamohammed72730882@gmail.com>
2024-08-27 19:44:27 -1200

    sidebar

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -63,35 +63,12 @@ const Sidebar = ({ isSidebarOpen,onLogout }) => {
             <span className="text">{isSidebarOpen ? 'Dashboard' : ''}</span>
           </Link>
         </li>
-        <li onClick={handleUsersClick} className={activeMenu === 'Users' ? 'active' : ''}>
-          <a href="#" onClick={() => handleMenuClick('Users')}>
+        <li className={activeMenu === 'Users' ? 'active' : ''}>
+          <Link to="userList" onClick={() => handleMenuClick('Users')}>
             <i className='bx bxs-group'></i>
             <span className="text">{isSidebarOpen ? 'Users' : ''}</span>
-            {showUsers ? <i className='bx bx-chevron-up'></i> : <i className='bx bx-chevron-down'></i>}
-          </a>
+          </Link>
         </li>
-        {showUsers && (
-          <ul className="nested">
-            <li className={activeMenu === 'User List' ? 'active' : ''}>
-              <Link to="/user-list" onClick={() => handleMenuClick('User List')}>
-              <i className='bx bxs-group'></i>
-                <span className="text">{isSidebarOpen ? 'User List' : ''}</span>
-              </Link>
-            </li>
-            <li className={activeMenu === 'New Users' ? 'active' : ''}>
-              <Link to="/new-users" onClick={() => handleMenuClick('New Users')}>
-                <i className='bx bxs-user-plus'></i>
-                <span className="text">{isSidebarOpen ? 'New Users' : ''}</span>
-              </Link>
-            </li>
-            <li className={activeMenu === 'Winners' ? 'active' : ''}>
-              <Link to="/winners" onClick={() => handleMenuClick('Winners')}>
-                <i className='bx bxs-star'></i>
-                <span className="text">{isSidebarOpen ? 'Winners' : ''}</span>
-              </Link>
-            </li>
-          </ul>
-        )}
         <li className={activeMenu === 'Payment' ? 'active' : ''}>
           <Link to="/payment" onClick={() => handleMenuClick('Payment')}>
             <i className='bx bxs-credit-card'></i>
